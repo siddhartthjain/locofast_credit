0587a90622cc8d04db40865bef235ee2111e7056
siddharth <siddhartthjain@gmail.com>
2023-03-31 17:39:05 +0530

    migration_returns_part2

--- src/_db/migrations/20230331120301_alter_fabric_order_table.ts
+++
@@ -1,9 +0,0 @@
-import * as Knex from 'knex';
-
-export async function up(knex: Knex): Promise<void> {
-  return knex.schema.alterTable('fabric_orders', function (table) {
-    table.dropColumn('credit_price');
-  });
-}
-
-export async function down(knex: Knex): Promise<void> {}
